8135e8a2a8b9e3647b6341e7787e4326355e06ee
Xu Bingbing <xubb@hotmail.com>
2018-11-11 14:10:44 +1100

    dev;vendor/bin/codecept g:feature acceptance checkout

---
+++ tests/acceptance/FirstCest.php
@@ -0,0 +1,14 @@
+<?php namespace App\Tests;
+use App\Tests\AcceptanceTester;
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
